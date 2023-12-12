6804acefced712efdad7b7abe36a9b89cd8b8805
Simon Biickert <simon@biickert.ca>
2023-12-12 14:56:37 -0700

    Day 12: Not good enough
    Need to change approach

--- Swift/AoC 2023/AoC 2023/Solutions/Day12.swift
+++ Swift/AoC 2023/AoC 2023/Solutions/Day12.swift
@@ -12,36 +12,58 @@ class Day12: AoCSolution {
         super.solve(input)
         
         let records = input.textLines.map {SpringRecord(s: $0)}
-        
-        let part1 = solvePartOne(records)
-        
-        return AoCResult(part1: "\(part1)", part2: "world")
-    }
-    
-    func solvePartOne(_ records: [SpringRecord]) -> Int {
-        return records.map({$0.possibleArrangementCount}).reduce(0, +)
+        let part1 = records.map({$0.possibleArrangementCount}).reduce(0, +)
+		
+		let expandedRecords = records.map { $0.expanded(times: 5) }
+//		print(expandedRecords)
+		var part2 = 0
+		for i in 0..<expandedRecords.count {
+			print(i)
+			part2 += expandedRecords[i].possibleArrangementCount
+			print(part2)
+		}
+
+        return AoCResult(part1: "\(part1)", part2: "\(part2)")
     }
 }
 
-enum SpringCondition: String {
-    case operational = "#"
-    case damaged = "."
-    case unknown = "?"
+enum SpringCondition {
+    case operational
+    case damaged
+    case unknown
+	
+	static func from(stringRepresentation s:String) -> SpringCondition {
+		switch s {
+		case "#":
+			return .operational
+		case ".":
+			return .damaged
+		default:
+			return .unknown
+		}
+	}
+	
+	var stringRepresentation: String {
+		switch self {
+		case .damaged: return "."
+		case .operational: return "#"
+		default: return "?"
+		}
+	}
 }
 
 struct SpringRecord: CustomDebugStringConvertible {
     let conditionRecord: [SpringCondition]
     let groupLengths: [Int]
-//    private let _re: Regex<Substring>
-    
+	private let originalConditionRecord: String
+	private let originalGroupLengths: String
+
     init(s: String) {
         let parts = s.split(separator: " ")
-        conditionRecord = parts[0].map {SpringCondition(rawValue: String($0))!}
+		conditionRecord = parts[0].map {SpringCondition.from(stringRepresentation: String($0))}
         groupLengths = parts[1].split(separator: ",").map {Int(String($0))!}
-//        let reGroups = groupLengths.map {"#{\($0)}"}
-//        let reStr = "^\\.*" + reGroups.joined(separator: "\\.+") + "\\.*$"
-//        //print(reStr)
-//        _re = try! Regex(reStr)
+		originalConditionRecord = String(parts[0])
+		originalGroupLengths = String(parts[1])
     }
     
     var possibleArrangementCount: Int {
@@ -52,30 +74,23 @@ struct SpringRecord: CustomDebugStringConvertible {
         let nTotalCount = 2 << (unknownIndexes.count - 1)
         //print("\(nTotalCount) combinations in \(self)")
         var count = 0
+		var altRecord = conditionRecord
         for bitNum in 0..<nTotalCount {
-            var altRecord = conditionRecord
             for i in 0..<unknownIndexes.count {
                 let index = unknownIndexes[i]
-                
                 let state: SpringCondition = getBit(x: i, in: bitNum) == 0 ? .damaged : .operational
-                //print("Setting [\(index)] to \(state). bits: \(bits)")
                 altRecord[index] = state
             }
-			if eval(record: altRecord) {
+			if evalAlternative(record: altRecord) {
 				//print("\(altRecord.map({$0.rawValue}).joined()) OK")
 				count += 1
 			}
-//            let altString = "\(altRecord.map({$0.rawValue}).joined())"
-//            if altString.contains(_re) {
-//                //print(altString)
-//                count += 1
-//            }
         }
         //print("\(count) of them are possible.")
         return count
     }
 	
-	private func eval(record r: [SpringCondition]) -> Bool {
+	private func evalAlternative(record r: [SpringCondition]) -> Bool {
 		var gIndex = 0
 		var currentGroupCount = 0
 		for ptr in 0..<r.count {
@@ -109,8 +124,19 @@ struct SpringRecord: CustomDebugStringConvertible {
 	private func getBit(x: Int, in num:Int) -> Int {
 		return num >> x & 1
 	}
+	
+	func expanded(times: Int) -> SpringRecord {
+		var condition = [String]()
+		var groups = [String]()
+		for _ in 0..<times {
+			condition.append(originalConditionRecord)
+			groups.append(originalGroupLengths)
+		}
+		let defn = "\(condition.joined(separator: "?")) \(groups.joined(separator: ","))"
+		return SpringRecord(s: defn)
+	}
     
     var debugDescription: String {
-        return "\(conditionRecord.map({$0.rawValue}).joined()) \(groupLengths)"
+		return "\(conditionRecord.map({$0.stringRepresentation}).joined()) \(groupLengths)"
     }
 }
