26c274e1836e9a441e17d5d81db31e74fea52561
Simon Biickert <simon@biickert.ca>
2023-12-15 15:03:58 -0700

    Day 1 parts 1 and 2 complete in Perl

--- Perl/day01.pl
+++ Perl/day01.pl
@@ -12,23 +12,71 @@ use AOC::Util;
 #use AOC::Geometry;
 #use AOC::Grid;
 
-my $INPUT_FILE = 'day01_test.txt';
-#my $INPUT_FILE = 'day01_challenge.txt';
+# my $INPUT_FILE = 'day01_test.txt';
+my $INPUT_FILE = 'day01_challenge.txt';
 my $INDEX = 0;
 
-my @input = read_grouped_input("../input/$INPUT_FILE", $INDEX);
+my @input;
 
 say "Advent of Code 2023, Day 01: Trebuchet!?";
 
-solve_part_one(@input);
-#solve_part_two(@input);
+@input = read_grouped_input("../input/$INPUT_FILE", 0);
+my $part1 = solve_part(@input);
+say "Part One: the sum of the calibration values is: $part1";
+
+if ($INPUT_FILE =~ m/test/) {
+	@input = read_grouped_input("../input/$INPUT_FILE", 1);
+}
+my @fixed_input = fix_input(@input);
+my $part2 = solve_part(@fixed_input);
+say "Part Two: the sum of the calibration values is: $part2";
 
 exit( 0 );
 
-sub solve_part_one(@input) {
-
+sub solve_part(@input) {
+	my $sum = 0;
+	for my $line (@input) {
+		$line =~ m/^[^\d]*(\d)/;
+		my $first_num = $1;
+		$line =~ m/(\d)[^\d]*$/;
+		my $last_num = $1;
+		my $num_string = $first_num . $last_num;
+		$sum += $num_string; # auto conversion to int
+	}
+	return $sum;
 }
 
-sub solve_part_two(@input) {
+sub fix_input(@input) {
+	my %lookup = ("one" => "1", "two" => "2", "three" => "3",
+        "four" => "4", "five" => "5", "six" => "6",
+        "seven" => "7", "eight" => "8", "nine" => "9");
 
+    my @fixed = ();
+    for my $line (@input) {
+    	# Trim from the start
+    	while (1) {
+			if ( $line =~ m/^\d/ ) { last; }
+			if ( $line =~ m/^(one|two|three|four|five|six|seven|eight|nine)/ ) {
+				my $digit = $lookup{$1};
+				$line =~ s/^$1/$digit/;
+			}
+			else {
+				$line = substr($line, 1);
+			}
+    	}
+    	# Trim from the end
+    	while (1) {
+			if ( $line =~ m/\d$/ ) { last; }
+			if ( $line =~ m/(one|two|three|four|five|six|seven|eight|nine)$/ ) {
+				my $digit = $lookup{$1};
+				$line =~ s/$1$/$digit/;
+			}
+			else {
+				$line = substr($line, 0, length($line)-1);
+			}
+    	}
+    	push(@fixed, $line);
+    }
+
+	return @fixed;
 }
