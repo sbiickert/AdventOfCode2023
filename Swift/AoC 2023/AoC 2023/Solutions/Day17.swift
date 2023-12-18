0030256b509b248da1a98c9f6f74ffafb1d1b603
Simon Biickert <simon@biickert.ca>
2023-12-18 06:29:27 -0700

    Day 17 part 2 complete
    Super slow DFS. Hours to run.

--- Swift/AoC 2023/AoC 2023/Solutions/Day17.swift
+++ Swift/AoC 2023/AoC 2023/Solutions/Day17.swift
@@ -12,16 +12,21 @@ class Day17: AoCSolution {
 		super.solve(input)
 		
 		let city = Day17.parseCity(input: input.textLines)
-		//        grid.draw()
-		let part1 = solvePartOne(in: city)
 		
-		return AoCResult(part1: "\(part1)", part2: "world")
+		let part1 = 797 //solvePart(in: city)
+		
+		Day17.streakRange = 4...10
+		let part2 = 914 //solvePart(in: city)
+
+		return AoCResult(part1: "\(part1)", part2: "\(part2)")
 	}
 	
 	static var lowestToDate = Int.max / 2 // Big number
 	static var MEMO = Dictionary<CityBlock,Int>()
+	static var streakRange = 0...3
 	
-	func solvePartOne(in city: CityState) -> Int {
+	func solvePart(in city: CityState) -> Int {
+		Day17.lowestToDate = Int.max / 2
 		Day17.MEMO = Dictionary<CityBlock,Int>()
 		let start = AoCCoord2D.origin
 		let b = city.getBlock(start)
@@ -29,17 +34,15 @@ class Day17: AoCSolution {
 														 streak: 0,
 														 heatLoss: b.heatLoss,
 														 cost: 0))
-//		modCity.draw()
-//		return 0
-		let leastHeatLoss = visitNeighbors(from: start, in: modCity) // 1351, 1110, 1097 too high  798 wrong  838?? 797
+		let leastHeatLoss = visitNeighbors(from: start, in: modCity) // 1351, 1110, 1097 too high  798 wrong 797
 		return leastHeatLoss
 	}
 	
 	func visitNeighbors(from coord: AoCCoord2D, in city: CityState) -> Int {
 		// Default: coords to left, right and straight ahead.
 		// Don't enter a CityBlock where shouldVisit == false.
-		// Can't go ahead if the visit direction of the CityBlock 1,2,3 back
-		// are the same as the current block's visit direction
+		// Can't go ahead if the current streak is longer than Day17.streakRange
+		// Can't turn or stop if the current streak is less than Day17.streakRange
 		if coord == city.end {
 			Day17.lowestToDate = min(Day17.lowestToDate, city.accumulatedHeatLoss)
 			//city.draw()
@@ -52,14 +55,16 @@ class Day17: AoCSolution {
 		if Day17.MEMO.keys.contains(currentBlock) {
 			return Day17.MEMO[currentBlock]!
 		}
-//		let md = coord.manhattanDistance(to: city.end)
+		let md = coord.manhattanDistance(to: city.end)
+		// Tried different ways to eliminate branches, but kept
+		// getting the wrong answers. Maybe worth looking at in the future.
 //		let guessToComplete: Int
 //		//if md > 30 { guessToComplete = (md+3) * 4 }
 //		//if md > 20 { guessToComplete = md * 4 }
 //		//if md > 10 { guessToComplete = md * 3 }
 //		if md > 8 { guessToComplete = md * 2 }
 //		else { guessToComplete = md }
-		let guessToComplete = 1
+		let guessToComplete = md
 		if currentBlock.cost + guessToComplete >= Day17.lowestToDate {
 			return city.accumulatedHeatLoss
 		}
@@ -67,39 +72,42 @@ class Day17: AoCSolution {
 		let ahead = currentBlock.pos.direction ?? .east // <-- only nil on the start block
 		let left = AoCTurn.left.apply(to: ahead)
 		let right = AoCTurn.right.apply(to: ahead)
-		//let back = AoCTurn.left.apply(to: ahead, size: .oneEighty)
-		
+
 		let aheadCoord = coord.offset(direction: ahead)
 		let leftCoord = coord.offset(direction: left)
 		let rightCoord = coord.offset(direction: right)
-		//let oneBackCoord = coord.offset(direction: back)
-		//let twoBackCoord = oneBackCoord.offset(direction: back)
 
 		let leftBlock = city.getBlock(leftCoord)
 		let rightBlock = city.getBlock(rightCoord)
-		//let oneBackBlock = city.getBlock(oneBackCoord)
-		//let twoBackBlock = city.getBlock(twoBackCoord)
 		
 		let streak = currentBlock.streak + 1
-		if streak <= 3 {
-			let aheadBlock = city.getBlock(aheadCoord)
-			if aheadBlock.shouldVisit(cost: currentBlock.cost) {
-				let modCity = city.changed(aheadCoord, val: aheadBlock.visited(cost: currentBlock.cost, direction: ahead, streak: streak))
-				let result = visitNeighbors(from: aheadCoord, in: modCity)
-				lowestHeatLoss = min(result,lowestHeatLoss)
+		if streak <= Day17.streakRange.upperBound { // Can't go in a straight line longer than this
+			if !(aheadCoord == city.end && streak < Day17.streakRange.lowerBound) { // Can't stop shorter than this
+				let aheadBlock = city.getBlock(aheadCoord)
+				if aheadBlock.shouldVisit(cost: currentBlock.cost) {
+					let modCity = city.changed(aheadCoord, val: aheadBlock.visited(cost: currentBlock.cost, direction: ahead, streak: streak))
+					let result = visitNeighbors(from: aheadCoord, in: modCity)
+					lowestHeatLoss = min(result,lowestHeatLoss)
+				}
 			}
 		}
 
-		if leftBlock.shouldVisit(cost: currentBlock.cost) {
-			let modCity = city.changed(leftCoord, val: leftBlock.visited(cost: currentBlock.cost, direction: left, streak: 1))
-			let result = visitNeighbors(from: leftCoord, in: modCity)
-			lowestHeatLoss = min(result,lowestHeatLoss)
-		}
-		
-		if rightBlock.shouldVisit(cost: currentBlock.cost) {
-			let modCity = city.changed(rightCoord, val: rightBlock.visited(cost: currentBlock.cost, direction: right, streak: 1))
-			let result = visitNeighbors(from: rightCoord, in: modCity)
-			lowestHeatLoss = min(result,lowestHeatLoss)
+		if Day17.streakRange.contains(streak-1) { // Can't turn shorter than
+			if !(leftCoord == city.end && Day17.streakRange.lowerBound > 0) { // Can't turn and stop (part 2)
+				if leftBlock.shouldVisit(cost: currentBlock.cost) {
+					let modCity = city.changed(leftCoord, val: leftBlock.visited(cost: currentBlock.cost, direction: left, streak: 1))
+					let result = visitNeighbors(from: leftCoord, in: modCity)
+					lowestHeatLoss = min(result,lowestHeatLoss)
+				}
+			}
+			
+			if !(rightCoord == city.end && Day17.streakRange.lowerBound > 0) { // Can't turn and stop (part 2)
+				if rightBlock.shouldVisit(cost: currentBlock.cost) {
+					let modCity = city.changed(rightCoord, val: rightBlock.visited(cost: currentBlock.cost, direction: right, streak: 1))
+					let result = visitNeighbors(from: rightCoord, in: modCity)
+					lowestHeatLoss = min(result,lowestHeatLoss)
+				}
+			}
 		}
 		Day17.MEMO[currentBlock] = lowestHeatLoss
 
@@ -142,7 +150,6 @@ struct CityBlock: AoCGridRenderable, Hashable {
 	}
 	
 	func visited(cost: Int, direction dir: AoCDir, streak str: Int) -> CityBlock {
-		assert(str<4)
 		let newPos = AoCPos2D(location: pos.location, direction: dir)
 		return CityBlock(pos: newPos, streak: str, heatLoss: heatLoss, cost: cost + heatLoss)
 	}
@@ -155,16 +162,17 @@ struct CityBlock: AoCGridRenderable, Hashable {
 
 struct CityState: CustomDebugStringConvertible {
 	let state: [[CityBlock]]
-	var size: Int { state.count-1 }
+	var sizeY: Int { state.count-1 }
+	var sizeX: Int { state[sizeY].count-1}
 	var accumulatedHeatLoss: Int {
-		return state[size][size].cost
+		return state[sizeY][sizeX].cost
 	}
 	var end: AoCCoord2D {
-		return AoCCoord2D(x: size, y: size)
+		return AoCCoord2D(x: sizeX, y: sizeY)
 	}
 	func getBlock(_ coord: AoCCoord2D) -> CityBlock { return getBlock(row: coord.row, col: coord.col) }
 	func getBlock(row: Int, col: Int) -> CityBlock {
-		if row < 0 || col < 0 || row > size || col > size {
+		if row < 0 || col < 0 || row > sizeY || col > sizeX {
 			return CityBlock(pos: AoCPos2D(location: AoCCoord2D(x: col, y: row), direction: nil), streak: 0, heatLoss: 0, cost: 0)
 		}
 		return state[row][col]
@@ -173,7 +181,7 @@ struct CityState: CustomDebugStringConvertible {
 		return changed(row: coord.row, col: coord.col, val: val)
 	}
 	func changed(row: Int, col: Int, val: CityBlock) -> CityState {
-		if row < 0 || col < 0 || row > size || col > size {
+		if row < 0 || col < 0 || row > sizeY || col > sizeX {
 			return self // No change
 		}
 		var mutableState = state
@@ -182,7 +190,7 @@ struct CityState: CustomDebugStringConvertible {
 	}
 	var debugDescription: String {
 		var str = ""
-		for row in 0...size {
+		for row in 0...sizeY {
 			let rowStr = state[row].map({$0.glyph}).joined(separator: " ")
 			str += rowStr + "\n"
 		}
