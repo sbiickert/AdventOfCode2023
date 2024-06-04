ebbd76128acc63acae71a6232d7b1b8f09e829b8
Simon Biickert <simon@biickert.ca>
2024-06-04 08:40:20 -0600

    Ruby Grid almost complete

--- Ruby/lib/grid.rb
+++ Ruby/lib/grid.rb
@@ -23,9 +23,19 @@ class Grid
 		@data[coord]
 	end
 	
-	#def get_scalar(coord)
-		# Not sure if this has value in Ruby
-	#end
+	def get_s(coord)
+		val = @data[coord]
+		if val.kind_of?(String) then
+			return val
+		elsif val.kind_of?(Array) then
+			return val.first
+		elsif val.kind_of?(Hash) then
+			return val[:glyph]
+		elsif val.respond_to?(:glyph) then
+			return val.glyph
+		end
+		return String(val)
+	end
 	
 	def set(coord, value)
 		@data[coord] = value
@@ -38,8 +48,81 @@ class Grid
 	
 	def clear(coord, reset_extent=false)
 		@data.delete(coord)
-		@extent = Extent.new(@data.keys) if reset_extent
+		@extent = Extent.new(*@data.keys) if reset_extent
 	end
 	
+	def coords(with_value=nil)
+		result = @data.keys
+		
+		if with_value != nil then
+			result.select! { |coord| @data[coord] == with_value }
+		end
+		
+		result
+	end
 	
+	def histogram(include_unset=false)
+		hist = Hash.new(default=0)
+		coords_to_summarize = []
+		if (include_unset) then
+			coords_to_summarize = @extent.all_coords.to_a
+		else
+			coords_to_summarize = self.coords
+		end
+		
+		coords_to_summarize.each do |c|
+			val = self.get_s(c)
+			hist[val] += 1
+		end
+		
+		return hist
+	end
+	
+	def neighbors(coord)
+		coord.get_adjacent_coords(@rule)
+	end
+	
+	def print(markers=nil, invert_y=false)
+		puts self.to_s(markers, invert_y)
+	end
+	
+	def to_s(markers=nil, invert_y=false)
+		return '' if @extent == nil
+		str = ''
+		
+		xmin = @extent.min.x
+		ymin = @extent.min.y
+		xmax = @extent.max.x
+		ymax = @extent.max.y
+		
+		y_indices = (ymin .. ymax).to_a
+		y_indices.reverse! if invert_y
+		
+		y_indices.each do |y|
+			row = []
+			for x in xmin .. xmax do
+				c = Coord.new(x, y)
+				glyph = self.get_s(c)
+				if markers != nil && markers[c] != nil then
+					glyph = markers[c]
+				end
+				row.push(glyph)
+			end
+			row.push("\n")
+			str += row.join('')
+		end
+		
+		str
+	end
+	
+	def load(rows)
+	
+	end
+end
+
+# Mixin
+module GridGlyph
+	def glyph
+		return @glyph
+	end
 end
--- Ruby/test/test_grid.rb
+++ Ruby/test/test_grid.rb
@@ -14,5 +14,66 @@ class TestGrid < Minitest::Test
 		g.set(Coord.new(1,1), 'A')
 		g.set(Coord.new(2,2), 'B')
 		g.set(Coord.new(4,4), 'D')
+		
+		g.set(Coord.new(1,4), {:glyph => 'E', 'type' => 'Elf', 'HP' => 100})
+		g.set(Coord.new(2,4), {:glyph => 'G', 'type' => 'Goblin', 'HP' => 95})
+		g.set(Coord.new(3,4), BasicGlyph.new('S'))
+		
+		#g.print
+		
+		assert_equal('A', g.get(Coord.new(1,1)))
+		assert_equal('B', g.get(Coord.new(2,2)))
+		assert_equal(g.default, g.get(Coord.new(3,3)))
+		assert_equal('D', g.get(Coord.new(4,4)))
+		
+		assert_equal('E', g.get_s(Coord.new(1,4)))
+		assert_equal('G', g.get_s(Coord.new(2,4)))
+		assert_equal('S', g.get_s(Coord.new(3,4)))
+		
+		ext = g.extent
+		assert_equal(Coord.new(1,1), ext.nw)
+		assert_equal(Coord.new(4,4), ext.se)
+		
+		all = g.coords
+		assert_equal(6, all.length) # Coords with 
+		matching = g.coords('B')
+		assert_equal(1, matching.length)
+		assert_equal(Coord.new(2,2), matching.first)
+		
+		g.set(Coord.new(3,3), 'B')
+		hist = g.histogram
+		assert(hist['A'] == 1 && hist['B'] == 2 && hist['.'] == 0)
+		hist = g.histogram(true)
+		assert(hist['A'] == 1 && hist['B'] == 2 && hist['.'] == 9)
+		
+		neighbors = g.neighbors(Coord.new(2,2))
+		assert_equal(4, neighbors.length)
+		assert_equal([Coord.new(2,1),Coord.new(3,2),Coord.new(2,3),Coord.new(1,2)], neighbors)
+		
+		g_str = g.to_s
+		assert_equal("A...\n.B..\n..B.\nEGSD\n", g_str)
+		g_str = g.to_s(nil,true)
+		assert_equal("EGSD\n..B.\n.B..\nA...\n", g_str)
+		
+		markers = {Coord.new(4,1) => '*'}
+		g_str = g.to_s(markers)
+		assert_equal("A..*\n.B..\n..B.\nEGSD\n", g_str)
+		
+		g.clear(Coord.new(3,3))
+		assert_equal(g.get(Coord.new(3,3)), g.default)
+		g.set(Coord.new(100,100), 'X')
+		big_ext = g.extent
+		assert_equal(Coord.new(100,100), big_ext.se)
+		g.clear(Coord.new(100,100), true)
+		small_ext = g.extent
+		assert_equal(ext, small_ext)
+	end
+end
+
+class BasicGlyph
+	include GridGlyph
+	
+	def initialize(glyph)
+		@glyph = glyph
 	end
 end
