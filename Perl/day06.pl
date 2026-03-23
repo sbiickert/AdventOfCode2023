7d13be657b5d892873746fa6e5cdc1e133e2f6cc
Simon Biickert <simon@biickert.ca>
2026-03-23 16:46:11 -0600

    Day 6 optimization
    Added a binary search function for part 2. More code, but 40x faster.

--- Perl/day06.pl
+++ Perl/day06.pl
@@ -43,12 +43,13 @@ sub solve_part_one() {
 
 sub solve_part_two(%race) {
 	my @win_counts = ();
-	my $count = get_winning_move_count(%race);
+	my $count = get_wmc_binary($race{'time'}, $race{'record'});
 
 	say "Part Two: the number of ways to win the combined race is $count";
 }
 
 sub get_winning_move_count(%race) {
+	# Search from start and finish
 	my $first_win = -1;
 	my $hold = 1;
 	while ($hold * ($race{'time'} - $hold) <= $race{'record'}) { $hold++; }
@@ -60,6 +61,47 @@ sub get_winning_move_count(%race) {
 	$last_win = $hold;
 
 	return $last_win - $first_win + 1;
+}
+
+sub get_wmc_binary($time, $record) {
+	# Binary search
+	my $lo = 1;
+	my $hi = $time;
+	my $ptr = $hi;
+
+	while (1) {
+		$ptr = int(($hi + $lo) / 2);
+		if (is_win($ptr, $time, $record)) {
+			$hi = $ptr;
+			last if !is_win($ptr-1, $time, $record);
+		}
+		else {
+			$lo = $ptr;
+		}
+	}
+	my $first_win = $ptr;
+
+	$lo = 1;
+	$hi = $time;
+	$ptr = 1;
+
+	while (1) {
+		$ptr = int(($hi + $lo) / 2);
+		if (is_win($ptr, $time, $record)) {
+			$lo = $ptr;
+			last if !is_win($ptr+1, $time, $record);
+		}
+		else {
+			$hi = $ptr;
+		}
+	}
+	my $last_win = $ptr;
+
+	return $last_win - $first_win + 1;
+}
+
+sub is_win($hold, $time, $record) {
+	return $hold * ($time - $hold) > $record;
 }
 
 sub parse_races(@input) {
