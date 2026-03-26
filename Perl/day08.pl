c4590837883f8d21d8d744151d4975a0477a33d4
Simon Biickert <simon@biickert.ca>
2026-03-26 16:43:15 -0600

    Day 8 part 2 complete in Perl

--- Perl/day08.pl
+++ Perl/day08.pl
@@ -11,50 +11,61 @@ use feature 'signatures';
 use Data::Printer;
 
 use AOC::Util;
-#use AOC::Geometry;
-#use AOC::Grid;
 
 my $DAY = sprintf("%02d", 8);
 my $INPUT = $ARGV[0];
 my $INPUT_FILE = 'day' . $DAY . '_' . $INPUT . '.txt';
-
-my @input = read_grouped_input("../input/$INPUT_FILE", 0);
 
 say "Advent of Code 2023, Day 8: Haunted Wasteland";
 
 my @turns;
 my %network;
 
-parse_input(@input);
+parse_input( read_grouped_input("../input/$INPUT_FILE", 0) );
 
 solve_part_one();
-#solve_part_two(@input);
+
+if ($INPUT eq 'test') {
+	parse_input(read_grouped_input("../input/$INPUT_FILE", 1));
+}
+
+solve_part_two();
 
 exit( 0 );
 
 sub solve_part_one() {
+	my $count = count_to_end("AAA", "ZZZ");
+	say "Part One: it took $count turns to get to ZZZ.";
+}
+
+sub solve_part_two() {
+	my %circuits = ();
+	for my $key (keys %network) {
+		if ($key =~ m/A$/) {
+			$circuits{$key} = count_to_end($key, 'Z$');
+		}
+	}
+	my $steps = lcm(values %circuits);
+	say "Part Two: all ghosts end on a Z after $steps steps.";
+}
+
+sub count_to_end($start, $end) {
 	my $p = 0;
-	my $node = "AAA";
+	my $node = $start;
 	my $count = 0;
-	while ($node ne "ZZZ") {
+	while (!($node =~ m/$end/)) {
 		$node = ($turns[$p] eq "L") ? $network{$node}[0] : $network{$node}[1];
 		$p++; $count++;
 		$p = $p % (scalar @turns);
 	}
-
-	say "Part One: it took $count turns to get to ZZZ.";
-}
-
-sub solve_part_two(@input) {
-
-	say "Part Two: ";
+	return $count;
 }
 
 sub parse_input(@input) {
 	@turns = split(//, shift(@input));
 	%network = ();
 	for my $line (@input) {
-		my @info = $line =~ m/([A-Z]{3})/g;
+		my @info = $line =~ m/([\w]{3})/g;
 		$network{$info[0]} = [$info[1], $info[2]];
 	}
 }
