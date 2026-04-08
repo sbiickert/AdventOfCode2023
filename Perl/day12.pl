90075e0355fa5a0b6b9fc6af7a590a610841fb8d
Simon Biickert <simon@biickert.ca>
2026-04-08 10:39:48 -0600

    Day 12 part 2 complete in Perl

--- Perl/day12.pl
+++ Perl/day12.pl
@@ -22,11 +22,12 @@ my @input = read_input("../Input/$INPUT_FILE");
 
 say "Advent of Code 2023, Day 12: Hot Springs";
 
-my @records = parse_records(@input);
+my @records = parse_records(1, @input);
 # p @records;
-
 solve_part_one(@records);
-#solve_part_two(@input);
+
+@records = parse_records(5, @input);
+solve_part_two(@records);
 
 exit( 0 );
 
@@ -37,25 +38,23 @@ sub solve_part_one(@records) {
 }
 
 sub solve_part_two(@input) {
-
-	say "Part Two: ";
+	my @counts = map { get_possible_arrangement_count( $_ ) } @records;
+	my $arr_count = sum(@counts);
+	say "Part Two: the total number of possible unfolded arrangements is $arr_count.";
 }
 
 my %memo = ();
 sub get_possible_arrangement_count($record) { # hash ref
 	%memo = ();
 	my $count = get_arr_count($record, 0, 0);
-	say "Result: $count";
 	return $count;
 }
 
 sub get_arr_count($record, $p, $g) {
 	my @groups = @{$record->{'groups'}};
 	my @status = @{$record->{'status'}};
-# 	say "($p, $g)" . join('', @status) . " " . join(',', @groups);
 	if ($g >= scalar @groups) { # no more groups
 		if ($p < $#status and (any {$_ eq '#'} @status[$p..$#status])) {
-# 			say "Still damaged springs";
 			return 0; # not a solution - there are still damaged springs in the record
 		}
 		return 1;
@@ -63,7 +62,6 @@ sub get_arr_count($record, $p, $g) {
 
 	my $g_length = $groups[$g];
 	if ($p + $g_length > scalar @status) {
-# 		say "Ran out of springs";
 		return 0; # we ran out of springs but there are still groups to arrange
 	}
 
@@ -76,9 +74,10 @@ sub get_arr_count($record, $p, $g) {
 		# if we can start group of damaged springs here
 		# eg: '??#...... 3' we can place 3 '#' and there is '?' or '.' after the group
 		# eg: '??##...... 3' we cannot place 3 '#' here
-# 		say $p . ".." . ($p + $g_length - 1);
 		my $b_no_operational = !any {$_ eq '.'} @status[$p..$p+$g_length-1];
+		no warnings 'uninitialized';
 		my $b_next_not_broken = $status[$p + $g_length] ne "#";
+		use warnings 'uninitialized';
 		if ($b_no_operational and $b_next_not_broken) {
 			# start damaged group here + this spring is operational ('.')
 			$result = get_arr_count($record, $p + $g_length + 1, $g + 1) +
@@ -91,9 +90,10 @@ sub get_arr_count($record, $p, $g) {
 	}
 	elsif ($status[$p] eq '#') {
 		# if we can start damaged group here
-# 		say $p . ".." . ($p + $g_length - 1);
 		my $b_no_operational = !any {$_ eq '.'} @status[$p..$p+$g_length-1];
+		no warnings 'uninitialized';
 		my $b_next_not_broken = ($status[$p + $g_length] ne "#");
+		use warnings 'uninitialized';
 		if ($b_no_operational and $b_next_not_broken) {
 			$result = get_arr_count($record, $p + $g_length + 1, $g + 1);
 		}
@@ -110,20 +110,17 @@ sub get_arr_count($record, $p, $g) {
 	return $result;
 }
 
-sub parse_records(@input) {
+sub parse_records($folds, @input) {
 	my @recs = ();
 	for my $line (@input) {
 		my @parts = split(/ /, $line);
-		my @status = split(//, $parts[0]);
+		my $status_str = join('?', ($parts[0]) x $folds);
+		my @status = split(//, $status_str);
 		push(@status, '.');
-		my @groups = split(/,/, $parts[1]);
-		my %info = ('status' => \@status, 'groups' => \@groups);#, 'regex' => mk_regex(@groups));
+		my $group_str = join(',', ($parts[1]) x $folds);
+		my @groups = split(/,/, $group_str);
+		my %info = ('status' => \@status, 'groups' => \@groups);
 		push(@recs, \%info);
 	}
 	return @recs;
 }
-
-# sub mk_regex(@groups) {
-# 	my @temp = map {"#{$_}"} @groups;
-# 	return join('\.+', @temp);
-# }
