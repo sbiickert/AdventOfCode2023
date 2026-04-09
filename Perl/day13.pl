76eb00166bcdd3cdb0689070e5f4cdfec9722c8a
Simon Biickert <simon@biickert.ca>
2026-04-09 15:43:34 -0600

    Day 13 optimization

--- Perl/day13.pl
+++ Perl/day13.pl
@@ -1,14 +1,11 @@
 #!/usr/bin/env perl
 use v5.42;
-# use feature 'class';
-# no warnings qw( experimental::class );
 
 our $directory;
 BEGIN { use Cwd; $directory = cwd; }
 use lib $directory . '/lib';
 
 use feature 'signatures';
-use Data::Printer;
 use List::Util 'sum';
 
 use AOC::Util;
@@ -33,7 +30,6 @@ exit( 0 );
 sub solve_part_one(@patterns) {
 	my @scores = ();
 	for my $pattern (@patterns) {
-# 		$pattern->print();
 		push(@scores, score_pattern($pattern));
 	}
 	my $total = sum @scores;
@@ -46,57 +42,54 @@ sub solve_part_two(@input) {
 		my $score;
 		my $original_score = score_pattern($pattern);
 		for my $coord ($pattern->extent()->all_coords()) {
-			my $value = $pattern->get_scalar($coord);
+			my $value = $pattern->get($coord);
 			$pattern->set($coord, ($value eq '#' ? '.' : '#'));
-# 			$pattern->print();
 			$score = score_pattern($pattern, $original_score);
 			$pattern->set($coord, $value);
 			if ($score > 0) {
-# 				say $coord->to_str();
 				last;
 			}
 		}
 		push(@scores, $score);
 	}
-# 	say join(',', @scores);
 	my $total = sum @scores;
 	say "Part Two: the total scores for the patterns after smudge fixed is $total.";
 }
 
-sub score_pattern($pattern, $orig = 0) {
+sub score_pattern($pattern, $orig = -1) {
 	my $idx = find_reflection_line_h($pattern, ($orig % 100 == 0) ? $orig / 100 : -1);
 	if ($idx > 0) {
-# 		say "H $idx";
 		my $score = $idx * 100;
 		return $score;
 	}
 	$idx = find_reflection_line_v($pattern, ($orig % 100 != 0) ? $orig : -1);
-# 	say "V $idx";
 	return $idx;
 }
 
 sub find_reflection_line_h($pattern, $orig_index) {
 	my @row_vals = ();
-	for my $row (0..$pattern->extent()->se()->Y()) {
-		my $row_chars = '0b';
-		for my $col (0..$pattern->extent()->se()->X()) {
-			my $val = $pattern->get_scalar(c2_make($col, $row));
-			$row_chars .= $val eq "#" ? '1' : '0';
+	my $ext = $pattern->extent();
+	for my $row (0..$ext->se()->Y()) {
+		my $row_chars = '';
+		for my $col (0..$ext->se()->X()) {
+			my $val = $pattern->get("[$col,$row]");
+			$row_chars .= $val;
 		}
-		push(@row_vals, oct($row_chars));
+		push(@row_vals, $row_chars);
 	}
 	return find_reflection_line($orig_index, @row_vals);
 }
 
 sub find_reflection_line_v($pattern, $orig_index) {
 	my @col_vals = ();
-	for my $col (0..$pattern->extent()->se()->X()) {
-		my $col_chars = '0b';
-		for my $row (0..$pattern->extent()->se()->Y()) {
-			my $val = $pattern->get_scalar(c2_make($col, $row));
-			$col_chars .= $val eq "#" ? '1' : '0';
+	my $ext = $pattern->extent();
+	for my $col (0..$ext->se()->X()) {
+		my $col_chars = '';
+		for my $row (0..$ext->se()->Y()) {
+			my $val = $pattern->get("[$col,$row]");
+			$col_chars .= $val;
 		}
-		push(@col_vals, oct($col_chars));
+		push(@col_vals, $col_chars);
 	}
 	return find_reflection_line($orig_index, @col_vals);
 }
@@ -104,13 +97,13 @@ sub find_reflection_line_v($pattern, $orig_index) {
 sub find_reflection_line($orig_index, @values) {
 	for my $ptr (0..$#values-1) {
 		next if $ptr+1 == $orig_index;
-		if ($values[$ptr] == $values[$ptr+1]) {
+		if ($values[$ptr] eq $values[$ptr+1]) {
 			# Potential reflection line. Work forwards and backwards to be sure.
 			my $b = $ptr;
 			my $f = $ptr+1;
 			my $good = true;
 			while ($b >= 0 and $f <= $#values) {
-				$good = ($good and ($values[$b] == $values[$f]));
+				$good = ($good and ($values[$b] eq $values[$f]));
 				$b--; $f++;
 			}
 			return $ptr+1 if $good;
