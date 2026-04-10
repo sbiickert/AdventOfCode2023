1e78d8a19cfe82896e8f57e03600a90f6515044d
Simon Biickert <simon@biickert.ca>
2026-04-10 14:11:17 -0600

    Day 14 part 2 complete in Perl

--- Perl/day14.pl
+++ Perl/day14.pl
@@ -7,7 +7,7 @@ use lib $directory . '/lib';
 
 use feature 'signatures';
 use Data::Printer;
-use List::Util 'sum';
+use List::Util qw(sum all);
 
 use AOC::Util;
 use AOC::Geometry;
@@ -22,26 +22,85 @@ my @input = read_input("../Input/$INPUT_FILE");
 say "Advent of Code 2023, Day 14: Parabolic Reflector Dish";
 
 solve_part_one(@input);
-#solve_part_two(@input);
+solve_part_two(@input);
 
 exit( 0 );
 
 sub solve_part_one(@input) {
 	my $platform = g2_make();
 	$platform->load(@input);
-# 	$platform->print();
 
 	roll_rocks($platform, "N");
 	my $total = measure_total_load($platform);
-
-# 	$platform->print();
 
 	say "Part One: the total load on the north is $total.";
 }
 
 sub solve_part_two(@input) {
+	my $platform = g2_make();
+	$platform->load(@input);
 
-	say "Part Two: ";
+	my $load = spin_cycle($platform);
+
+	say "Part Two: the load on the north after a spin cycle is $load.";
+}
+
+sub spin_cycle($platform) {
+	# Go until steady state
+	my @loads = ();
+	my %load_indexes = ();
+	my $cycle = 0;
+	my $limit = 1000000000;
+	my $start_of_cycle = 0;
+	for my $i (1..$limit) {
+		roll_rocks($platform, "N");
+		roll_rocks($platform, "W");
+		roll_rocks($platform, "S");
+		roll_rocks($platform, "E");
+		my $load = measure_total_load($platform);
+		push(@loads, $load);
+		$load_indexes{$load} = [] if !defined($load_indexes{$load});
+		push(@{$load_indexes{$load}}, $i);
+		if (scalar @{$load_indexes{$load}} > 2) {
+			$cycle = find_cycle(\@loads);
+		}
+		say $i; # . " " . join(',', @loads);
+		if ( $cycle > 0 ) {
+			say "found cycle of $cycle after $i spins.";
+			say join(',', @loads);
+			$start_of_cycle = $i;
+			last;
+		}
+	}
+
+	my $remainder = ($limit - $start_of_cycle) % $cycle;
+# 	say "$remainder = ($limit - $start_of_cycle) % $cycle";
+	my $idx = $#loads - $cycle + $remainder;
+# 	say "$idx = $#loads - $cycle + $remainder";
+	return $loads[$idx];
+}
+
+sub find_cycle($loads_ref) {
+	my @loads = @{$loads_ref};
+	my $ptr = $#loads-1;
+
+	while ($ptr > $#loads / 2) {
+		if ($loads[$ptr] == $loads[-1]) {
+			my $off = 1;
+			my $i = $ptr - $off; my $j = $#loads - $off;
+			while ($i >= 0 and $loads[$i] == $loads[$j]) {
+
+				$off++;
+				$i = $ptr - $off; $j = $#loads - $off;
+			}
+			if ($i >= 0 && $j == $ptr-1) {
+				return $off-1;
+			}
+		}
+		$ptr--;
+	}
+
+	return 0;
 }
 
 sub measure_total_load($platform) {
