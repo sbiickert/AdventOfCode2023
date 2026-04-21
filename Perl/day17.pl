1bd7d7412d9807899726a17e5c6f07596b8d1934
Simon Biickert <simon@biickert.ca>
2026-04-21 17:59:44 -0600

    Day 17 complete in Perl
    Optimized with Claude Code

--- Perl/day17.pl
+++ Perl/day17.pl
@@ -1,7 +1,5 @@
 #!/usr/bin/env perl
 use v5.42;
-# use feature 'class';
-# no warnings qw( experimental::class );
 
 our $directory;
 BEGIN { use Cwd; $directory = cwd; }
@@ -9,6 +7,7 @@ use lib $directory . '/lib';
 
 use feature 'signatures';
 use Data::Printer;
+use Heap::PQ;
 
 use AOC::Util;
 use AOC::Geometry;
@@ -26,157 +25,71 @@ my $city = g2a_make(length($inputs[0][0]) - 1, $#{$inputs[0]});
 $city->load(@{$inputs[0]});
 # $city->print();
 
-solve_part_one($city);
-#solve_part_two(@input);
+my $p1 = solve_part($city, 1, 3);
+say "Part One: the least heat loss is $p1.";
+my $p2 = solve_part($city, 4, 10);
+say "Part Two: the least heat loss is $p2.";
 
 exit( 0 );
 
-sub solve_part_one($city) {
+sub solve_part($city, $min_run, $max_run) {
 	my %start = ('dir' => 'E', 'run' => 1, 'loss' => 0, 'id' => '0');
 
-	my $best = 800;
+	my $best = 1000;
 	my $best_id;
 	my $dest_x = $city->x_max();
 	my $dest_y = $city->y_max();
 	my $loss_at_destination = $city->get_xy($dest_x, $dest_y);
 
-	# Set up grid for tracking results
-	my $grid = g2a_make($dest_x, $dest_y);
-	my ($xs, $ys) = $grid->xy();
-	for my $i (0..$#{$xs}) {
-		my %empty = ();
-		$grid->set_xy($xs->[$i], $ys->[$i], \%empty);
-	}
-	my $ref = $grid->get_xy(0, 0);
-	my $key = $start{'dir'} . " " . $start{'run'};
-	$ref->{$key} = \%start;
+	my $work_heap = Heap::PQ::new('min', sub {$a->[2] <=> $b->[2]});
 
-	my @work = ([0,0,0]); # x,y,loss
-	while (scalar(@work) > 0) {
-		my @next_work = ();
-		while (my $job = pop(@work)) {
-			my $x = $job->[0]; my $y = $job->[1]; my $loss = $job->[2];
+	$work_heap->push([0,0,0,'E',1]); # x,y,loss,dir,run
+	my %closed;
+	my %grid;
+	while (my $job = $work_heap->pop()) {
+		my ($x,$y,$loss,$dir,$run) = @{$job};
+		my $closed_key = "$x,$y,$dir,$run";
+		next if exists $closed{$closed_key};
+		$closed{$closed_key} = 1;
 
-			if ($x == $dest_x && $y == $dest_y) {
-				if ($loss < $best) {
-					$best = $loss ;
-					$best_id = $job->[3];
-					say $best;
-				}
-				next;
-			}
-			my $heur = (2.5 * ($dest_x - $x) + ($dest_y -$y));
-			next if $loss >= $best - $heur;
+		if ($x == $dest_x && $y == $dest_y) {
+			$best = $loss;
+			last;
+		}
 
-# 			say "$x, $y";
-			my @states = values %{$grid->get_xy($x, $y)};
-			for my $state (@states) {
-				my $id = $state->{'id'};
-				my $state_job = [$x, $y, $state->{'loss'}, $id];
-				# Go straight
-				if ($state->{'run'} < 3) {
-					my $new_job = eval_dir($state->{'dir'}, $city, $grid, $state->{'run'}, $state_job, $id);
-					if (defined $new_job) {
-						push( @next_work, $new_job);
-					}
-				}
-				# Turn left
-				my $dir_ccw = Position->turn_str("CCW", $state->{'dir'});
-				my $new_ccw_job = eval_dir($dir_ccw, $city, $grid, 0, $state_job, $id);
-				if (defined $new_ccw_job) {
-					push( @next_work, $new_ccw_job);
-				}
+		if ($run < $max_run) {
+			my $new_job = eval_dir($dir, $city, \%grid, $run, $job);
+			$work_heap->push($new_job) if defined $new_job;
+		}
 
-				# Turn right
-				my $dir_cw = Position->turn_str("CW", $state->{'dir'});
-				my $new_cw_job = eval_dir($dir_cw, $city, $grid, 0, $state_job, $id);
-				if (defined $new_cw_job) {
-					push( @next_work, $new_cw_job);
-				}
+		if ($run >= $min_run) {
+			for my $rot ('CW','CCW') {
+				my $new_dir = Position->turn_str($rot, $dir);
+				my $new_job = eval_dir($new_dir, $city, \%grid, 0, $job);
+				$work_heap->push($new_job) if defined $new_job;
 			}
 		}
-# 		p @work;
-# 		my $pause = <STDIN>;
-		@work = sort { $a->[0] + $a->[1] <=> $b->[0] + $b->[1] } @next_work; # ascending by manhattan distance from start
-# 		say "----";
-		@next_work = ();
 	}
 
-# 	say $best_id;
-# 	my %markers = mk_debug_markers($grid, $best_id);
-# 	p %markers;
-# 	$city->print(\%markers);
-
-	say "Part One: the least heat loss is $best.";
+	return $best;
 }
 
-my $next_id = 0;
-sub get_next_id() { return ++$next_id }
-
-sub eval_dir($dir, $city, $grid, $run, $job, $id) {
+sub eval_dir($dir, $city, $grid, $run, $job) {
+	my ($jx, $jy, $loss) = @{$job};
 	my @offset = xy_offset($dir);
-	my $off_x = $job->[0] + $offset[0];
-	my $off_y = $job->[1] + $offset[1];
+	my $off_x = $jx + $offset[0];
+	my $off_y = $jy + $offset[1];
 
 	my $new_job;
-	if ($off_x >= 0 && $off_x <= $city->x_max() &&
-		$off_y >= 0 && $off_y <= $city->y_max()) {
-		my $loss = $job->[2];
-		my $loss_at_offset = $city->get_xy($off_x, $off_y);
-		my $new_id = $id . ',' . get_next_id;
-		$new_job = [$off_x, $off_y, $loss + $loss_at_offset, $new_id];
-		my %new_state = ('dir' => $dir,
-						 'run' => $run+1,
-						 'loss' => $loss + $loss_at_offset,
-						 'id' => $new_id);
+	return $new_job unless ($off_x >= 0 && $off_x <= $city->x_max()
+						 && $off_y >= 0 && $off_y <= $city->y_max());
 
-		my $ref = $grid->get_xy($off_x, $off_y);
-		my $key = "$dir " . ($run + 1);
-		if (defined $ref->{$key}) {
-			# Have re-entered a block from the same direction
-			if ($loss + $loss_at_offset < $ref->{$key}{'loss'}) {
-				$ref->{$key} = \%new_state;
-			}
-			else {
-				$new_job = undef;
-			}
-		}
-		else {
-			$ref->{$key} = \%new_state;
-		}
-	}
+	my $loss_at_offset = $loss + $city->get_xy($off_x, $off_y);
+	my $key = "$off_x,$off_y,$dir," . ($run + 1);
+
+	return $new_job if exists $grid->{$key} && $grid->{$key} <= $loss_at_offset;
+	$grid->{$key} = $loss_at_offset;
+
+	$new_job = [$off_x, $off_y, $loss_at_offset, $dir, $run+1];
 	return $new_job;
-}
-
-sub mk_debug_markers($grid, $best_id) {
-	my %markers = ();
-
-	my @ids = split(/,/, $best_id);
-	my %id_lookup = ();
-	while ( scalar(@ids) > 0 ) {
-		my $id = join(',', @ids);
-		$id_lookup{$id} = 1;
-		pop(@ids);
-	}
-	p %id_lookup;
-	my %char_map = ('S' => 'v', 'N' => '^', 'W' => '<', 'E' => '>');
-
-	my @coords = $grid->coords();
-	for my $id (keys %id_lookup) {
-		for my $c (@coords) {
-			my @states = values %{$grid->get($c)};
-			for my $state (@states) {
-				if (defined $id_lookup{$state->{'id'}}) {
-					$markers{$c->to_str()} = $char_map{ $state->{'dir'} };
-				}
-			}
-		}
-	}
-
-	return %markers;
-}
-
-sub solve_part_two(@input) {
-
-	say "Part Two: ";
 }
